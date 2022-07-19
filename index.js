092d5bf383c555e3daffbcdfab42c9514e9ffe26
wali <wali.r@mytechnology-team.com>
2022-07-19 18:08:40 +0500

    node

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const data = require('./data')
 const fs = require('fs')
 const http=require('http')
 const upperCase=require('upper-case')
-
-const express= require('express')
-
+const express = require('express')
 const colors=require('colors')
+const paths=require('path')
+var ejs = require('ejs');
 
 console.log(app.z)
 console.log("hi world")
@@ -62,19 +62,50 @@ for(i=0; i<5;i++)
   // fs.writeFileSync(dirPath+"/file"+i+".txt", "file created")
 }
 
-//making pages
+//making html pages
 const newApp= express();
-newApp.get('/home', (req,res)=>{
-  res.send('<h1>HTML here</h1>')
-  res.send("hello bro, this is home page")
+// newApp.get('/home', (req,res)=>{
+//   res.send('<h1>HTML here</h1>')
+//   res.send("hello bro, this is home page")
+// })
+// newApp.get('/about', (req,res)=>{
+//   res.send("hello bro this is about page")
+// })
+// newApp.get('/contact', (req,res)=>{
+//   res.send("hello bro this is Contact page")
+//   // res.send('<h1>Name is : ${req.query.name}</h1>')
+//  // http://localhost:4300/contact?name=Wali
+//   console.log(req.query.name)
+// })
+// newApp.listen(4300)
+
+
+const publicPath=paths.join(__dirname,'public')
+console.log("the path is : " + publicPath);
+newApp.use(express.static(publicPath))
+
+newApp.set('view engine', 'ejs')
+
+newApp.get('/match', (_,resp)=>{
+  resp.sendFile(`${publicPath}/match.html`)
 })
-newApp.get('/about', (req,res)=>{
-  res.send("hello bro this is about page")
+
+newApp.get('/profile', (_,resp)=>{
+  
+  const user={
+    name:"Wali ur Rehman",
+    age:26,
+    skills:['c++','php','laravel']
+  }
+  resp.render('profile',{user})
 })
-newApp.get('/contact', (req,res)=>{
-  res.send("hello bro this is Contact page")
-  // res.send('<h1>Name is : ${req.query.name}</h1>')
- // http://localhost:4300/contact?name=Wali
-  console.log(req.query.name)
+
+//make ejs file and pass data in it
+newApp.get('*', (_,resp)=>{
+
+  resp.sendFile(`${publicPath}/404.html`)
 })
-newApp.listen(4300)
+
+
+
+newApp.listen(5000)
